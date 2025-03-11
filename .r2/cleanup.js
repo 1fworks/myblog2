1c89b6a83b9c2f1977a55ca469ad81d4497fa06b
1fworks <1fworks@github.com>
2025-03-12 04:26:48 +0900

    change .r2 file name

--- .r2/cleanup.js
+++ .r2/cleanup.js
@@ -1,9 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { sync } from 'glob';
-import { main as movfolder } from './movfolder';
-
-movfolder()
+import './movfolder.js';
 
 const list = sync(`${path.join(process.cwd(), '/public')}/**/nextImageExportOptimizer`, { posix:true, dotRelative:true })
 list.forEach(path=>{
